84115408756d1d74c490339196c001c59921dd42
marianagonzl <10193@portalcedi.edu.mx>
2024-05-21 13:54:17 -0600

    jugadores, imagen

--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
 #include <SFML/Graphics.hpp>
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace sf;
 using namespace std;
 
+
+
 int main()
 {
-    RenderWindow window(VideoMode(400, 400), "Ohaio onichan!");
-    Texture yoshiTexture;
-    if(!yoshiTexture.loadFromFile("assets/Yoshi.png"))
+    RenderWindow window(VideoMode(800, 400), "Ohaio onichan!");
+    window.setFramerateLimit(60);
+    Texture TaylorTexture;
+
+    if(!TaylorTexture.loadFromFile("assets/yoshinobg.png"))
     {
-        cout << "Error kawaii onichan" << endl;
-        system("pause");
+        cout << "Error al cargar imagen" << endl;
     }
+    TaylorTexture.setRepeated(true);
 
-    Sprite yoshi;
-    yoshi.setTexture(yoshiTexture);
-    yoshi.setTextureRect(IntRect(0,0,800,180));
-    yoshi.setColor(Color(255,255,255));
-
+    Sprite Taylor;
+    Taylor.setTexture(TaylorTexture);
+    Taylor.setTextureRect(IntRect(0, 0, Cx, cy));
+    int opacidad = 0;
+    
     while (window.isOpen())
     {
         Event event;
@@ -27,10 +31,14 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(yoshi);
+        window.clear(Color(51,51,51));
+        window.draw(Taylor);
+        Taylor.setColor(Color(255,255,255));
         window.display();
     }
 
     return 0;
 }
+
+
+
--- mainlol.cpp
+++ mainlol.cpp
@@ -5,12 +5,85 @@ using namespace sf;
 using namespace std;
 #include "presentacion.hpp"
 
+void menuJugador(Jugador *player);
+Jugador *player;
+Juego GuessTheCelebrity("GuessTheCelebrity");
+
+int menuprincipal()
+{
+    Instruc();
+    int num;
+    inicio:
+    cout << "Has jugado antes? " << endl;
+    cout << "1. Si" << endl;
+    cout << "2. No" << endl;
+    cin >> num;
+    system("cls");
+    switch (num)
+    {
+    case 1:
+        int idusu;
+        cout << "Escribe tu id para identificación: " << endl;
+        cin >> idusu;
+
+        jugador = GuessTheCelebrity.getUsuario(idusu);
+        if (jugador != nullptr)
+        {
+            menuJugador(jugador);
+        }
+
+        goto ;
+        break;
+
+        case 2:
+        cout << "Agregar un nuevo usuario" << endl;
+        GuessTheCelebrity.agregarUsuario();
+        goto menuJugador;
+        break;
+
+    /*case 1:
+        cout << "Listado de todos los usuarios que existen y sus puntajes maximos" << endl;
+        GuessTheCelebrity.mostrarUsuarios();
+        goto inicio;
+        break;*/
+
+    default:
+        cout << "Ingresa una de las opciones validas" << endl;
+        goto inicio;
+    }
+fin:
+    return 0;
+}
+
+void menuJugador(Jugador *player)
+{
+    int num;
+
+    cout << "Bienvenido a Famous crushes" << endl;
+    inicio:
+    cout << "Introduce un numero para realizar una de las siguientes acciones: " << endl;
+    cout << "1. Jugar" << endl;
+    cout << "0. Salir" << endl;
+    cin >> num;
+    system("cls");
+    switch (num)
+    {
+    case 1:
+        usu->mostrarAmigos();
+        goto inicio;
+        break;
+
+    case 0:
+        menuprincipal();
+        break;
+
+    default:
+        cout << "Ingresa una de las opciones validas" << endl;
+        goto inicio;
+    }
+
 int main()
 {
-
-    //presentacion();
-    Instruc();
-    /*usuario();
-    jugar();*/
+    menuprincipal();
     return 0;
 }
